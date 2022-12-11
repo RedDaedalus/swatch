cdeb3df1e63891e436f91939caad802305b7350e
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2022-12-11 16:05:12 -0700

    chore: code quality improvements

--- src/utils.rs
+++ src/utils.rs
@@ -10,3 +10,19 @@ cfg_if! {
         pub fn set_panic_hook() {}
     }
 }
+
+pub fn compose_svg(color: &String) -> String {
+    return format!(r##"
+        <svg width="512" height="256" viewbox="0 0 512 256" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <linearGradient id="g" x2="100%" y2="100%">
+                    <stop offset="0%" stop-color="black" stop-opacity="0" />
+                    <stop offset="100%" stop-color="black" stop-opacity="0.4" />
+                </linearGradient>
+            </defs>
+
+            <rect rx="4" ry="4" width="512" height="256" fill="{}" />
+            <rect rx="4" ry="4" width="512" height="256" fill="url(#g)" />
+        </svg>
+    "##, color);
+}
